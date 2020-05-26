ec9ddb56a53666c320979d3ca78e1fcadd4f4701
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-05-25 19:36:05 -0500

    fix styles

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,15 +5,13 @@ import PrivateRoutes from './private/PrivateRoutes';
 import Navbar from '../shared/Navbar/Navbar';
 
 function AppRouter() {
-  const auth = true;
-  return (
-    <>      
-      <Router>
-        <Navbar auth={auth}/>
-        <PublicRoutes auth={auth} />
-        <PrivateRoutes auth={auth} />
-      </Router>
-    </>
+  const auth = false;
+  return (     
+    <Router>
+      <Navbar auth={auth}/>
+      <PublicRoutes auth={auth} />
+      <PrivateRoutes auth={auth} />
+    </Router>
   );
 }
 
