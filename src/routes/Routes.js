6ee41087f99460ba34e9d909a33298fe0ff3d213
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-05-25 19:37:59 -0500

    fix styles

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,7 +6,7 @@ import Navbar from '../shared/Navbar/Navbar';
 
 function AppRouter() {
   const auth = false;
-  return (     
+  return (
     <Router>
       <Navbar auth={auth}/>
       <PublicRoutes auth={auth} />
