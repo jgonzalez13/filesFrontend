08155bdd3b9c26a7993d9284c14e0b96e6a31a7b
armanirmr <sbuenrostro@ucol.mx>
2020-06-03 17:54:00 -0500

    Formulario de Audiencias

--- src/views/Public/Home/Home.jsx
+++ src/views/Public/Home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Banner from './components/Banner/Banner';
+import Icon  from '../../../shared/Icon/Icon';
 // Modal here
 
 const Home = () => {
   return (
     <>
-      <Banner />
+      <Banner />      
       <div>
         <h1 className="title">HomeView</h1>
         <button class="button is-primary">Button</button>
+        <Icon name = "fas fa-home"/>
       </div>
     </>
   );
